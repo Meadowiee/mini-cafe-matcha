8395f0dc68070bafd9651404fddd8163f98762fc
zusnia <zusniakhalifatussakdia@gmail.com>
2025-05-11 08:38:17 +0800

    first push

--- indexes.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head><title>Landing Page</title></head>
-<body>
-    <h1>Welcome to Mini Cafe Matcha</h1>
-    <a href="signup.php">Sign Up</a> |
-    <a href="login.php">Login</a>
-</body>
-</html>
